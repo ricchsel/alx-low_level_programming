486a2b9a6943528ee87c69c8b3e7a1d57414f555
Richmond Sel <ricchsel@gmail.com>
2022-10-17 10:32:43 -0700

    Update printf file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	printf("with the proper grammer, but the outcome is a piece of art,\n");
-	return 0;
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
 }
