d57d6182f26d766fd566178aa8e9225572a7c75c
Richmond Sel <ricchsel@gmail.com>
2022-10-18 21:03:01 -0700

    Update putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
  */
-
 int main(void)
 {
-	printf("_putchar\n");
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
