65935e649d3a38cd251101ffe229ec42d1b4417d
ricchsel <114527950+ricchsel@users.noreply.github.com>
2022-10-21 06:36:06 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,33 +6,18 @@
 
 void print_number(int n)
 {
-	long len, res, i, temp, expo;                                                                                                                
-                                                                                                                                                     
-        res = n;                                                                                                                                     
-        expo = len =  1;                                                                                                                             
-/*Check negatives*/                                                                                                                                  
-        if (res < 0)                                                                                                                                 
-        {                                                                                                                                            
-                res *= -1;                                                                                                                           
-                _putchar('-');                                                                                                                       
-        }                                                                                                                                            
-                                                                                                                                                     
-/**/                                                                                                                                                 
-        temp = res;                                                                                                                                  
-        while (temp >= 10)                                                                                                                           
-        {                                                                                                                                            
-                len++;                                                                                                                               
-                temp /= 10;                                                                                                                          
-        }                                                                                                                                            
-                                                                                                                                                     
-/*Create Exponent*/                                                                                                                                  
-        for (i = 1; i < len; i++)                                                                                                                    
-                expo *= 10;                                                                                                                          
-/*Main */                                                                                                                                            
-        while (expo > 1)                                                                                                                             
-        {                                                                                                                                            
-                _putchar((res / expo) % 10 + '0');                                                                                                   
-                expo /= 10;                                                                                                                          
-        }                                                                                                                                            
-        _putchar(res % 10 + '0');
+/*Check negatives*/
+	if (n < 0) 
+	{
+		putchar('-');
+		n = -n;
+        }
+/**/
+        if (n == 0)
+		putchar('0');
+
+        if (n/10)
+		printnumber(n/10);
+
+    	putchar(n%10 + '0');                                                                                                                                                                                                                                          
 }
